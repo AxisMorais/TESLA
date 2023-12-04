0d075f36ebfc855f1fe9743c04ecbff9bb3f14ee
ThiagoMorais <89652858+AxisMorais@users.noreply.github.com>
2023-12-04 15:12:12 -0300

    Update CAT.py

--- CAT.py
+++ CAT.py
@@ -2,29 +2,30 @@ from datetime import time, datetime
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-#from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 import datetime
 from datetime import datetime
+from selenium import webdriver 
 
 class ExecutorCargaCAT:
-
+    
     def executarCargaCat(quantidade):
-
+    
         for x in range(0, quantidade):
+            
+            #Inicia o webdriver 
+            driver = webdriver.chrome
 
-            # Variável serviço para conectar o selenium ao Google Chrome e chamar o serviço webdriver
-            servico = Service(ChromeDriverManager().install())
-
-            navegador = webdriver.Chrome(service=servico)
+            navegador = webdriver.Chrome()
 
             navegador.get('http://cic.pbh/')
 
             campo_username = navegador.find_element(by='name', value='josso_username')
-            campo_username.send_keys('Inserir login de acesso')
+            campo_username.send_keys('thiago.conegundes')
 
             campo_senha = navegador.find_element(by='name', value='josso_password')
-            campo_senha.send_keys('Inserir senha de acesso')
+            campo_senha.send_keys('Th1505@')
 
             # Clicar no botão para acessar o sistema:
             navegador.find_element(By.CLASS_NAME, "botao").click()
@@ -94,14 +95,8 @@ class ExecutorCargaCAT:
             time.sleep(5)
 
             # Clicar no botão Gravar
-            navegador.find_element(By.ID, "gravar").click()
+            #navegador.find_element(By.ID, "gravar").click()
 
             # Esperar 5 segundos para sair
             time.sleep(3)
             navegador.quit()
-
-
-
-meuObjetoTeste = ExecutorCargaCAT
-quantidade=(int(input("Informe a quantidade de cargas que deseja executar \n")))
-meuObjetoTeste.executarCargaCat(quantidade)
